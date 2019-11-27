131c80bb80de6e37ea2c91c60cb32fc548977190
Maximilian Sindram <msindram@virtualcitysystems.de>
2019-11-27 18:23:28 +0100

    add sout for all elements to main

--- src/main/java/de/vcs/test/Main/Main.java
+++ src/main/java/de/vcs/test/Main/Main.java
@@ -11,15 +11,19 @@ import java.io.File;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        System.out.println("Max");
+        System.out.println("----------------------------------------");
         XMLObjects xmlObjects = XMLObjects.newInstance();
         XMLReaderFactory factory = XMLReaderFactory.newInstance(xmlObjects);
         Verzeichnis verzeichnis;
         try (XMLReader reader = factory.createReader(new File("src/main/resources/test.xml"))) {
             verzeichnis = xmlObjects.fromXML(reader, Verzeichnis.class);
         }
+        System.out.println(verzeichnis.getTitel().getName());
         for (Eintrag e : verzeichnis.getEinraege()) {
+            System.out.println(e.getStadt().getName());
+            System.out.println(e.getBeschreibung().getText());
             System.out.println(e.getPoint().getPos().getValue());
         }
+        System.out.println("----------------------------------------");
     }
 }
