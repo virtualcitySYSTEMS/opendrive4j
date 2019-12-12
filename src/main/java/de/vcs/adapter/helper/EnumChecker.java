a4a8e81d41f840f75e8bc3b39410c049d9cf6df7
Maximilian Sindram <msindram@virtualcitysystems.de>
2019-12-12 16:08:54 +0100

    removed unused method in EnumChecker.

--- src/main/java/de/vcs/adapter/helper/EnumChecker.java
+++ src/main/java/de/vcs/adapter/helper/EnumChecker.java
@@ -10,10 +10,4 @@ public class EnumChecker {
         return e.stream().anyMatch(x -> Arrays.stream(x.getEnumConstants())
                 .anyMatch(y -> value.equals(((Enum) y).name())));
     }
-
-    public static boolean contains3(String value, ArrayList<Class<? extends Enum<?>>> e
-    ) {
-        return e.stream().anyMatch(x -> Arrays.stream(x.getEnumConstants())
-                .anyMatch(y -> value.equals(((Enum) y).name())));
-    }
 }
