b1a0f9d07ec3b6ee81ce9425ce6cdd80c123945c
Maximilian Sindram <msindram@virtualcitysystems.de>
2019-12-12 16:44:12 +0100

    change name() to toString() in EnumChecker. Enables special character pattern in Enum

--- src/main/java/de/vcs/adapter/helper/EnumChecker.java
+++ src/main/java/de/vcs/adapter/helper/EnumChecker.java
@@ -8,6 +8,12 @@ public class EnumChecker {
     public static boolean contains(String value, ArrayList<Class<? extends Enum<?>>> e
     ) {
         return e.stream().anyMatch(x -> Arrays.stream(x.getEnumConstants())
-                .anyMatch(y -> value.equals(((Enum) y).name())));
+                .anyMatch(y -> value.equals(((Enum) y).toString())));
+    }
+
+    public static boolean contains(String value, Class<? extends Enum<?>> e
+    ) {
+        return Arrays.stream(e.getEnumConstants())
+                .anyMatch(y -> value.equals(((Enum) y).toString()));
     }
 }
