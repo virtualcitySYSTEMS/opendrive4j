1e6d6bf9ad9ded4d66dcb2c97e2550999220559b
Maximilian Sindram <msindram@virtualcitysystems.de>
2019-12-12 17:45:29 +0100

    minor changes

---
+++ src/main/java/de/vcs/adapter/signal/DependencyAdapter.java
@@ -0,0 +1,25 @@
+package de.vcs.adapter.signal;
+
+import de.vcs.model.odr.signal.Dependency;
+import org.xmlobjects.builder.ObjectBuildException;
+import org.xmlobjects.builder.ObjectBuilder;
+import org.xmlobjects.stream.XMLReadException;
+import org.xmlobjects.stream.XMLReader;
+import org.xmlobjects.xml.Attributes;
+
+import javax.xml.namespace.QName;
+
+public class DependencyAdapter implements ObjectBuilder<Dependency> {
+
+    @Override
+    public Dependency createObject(QName name) throws ObjectBuildException {
+        return new Dependency();
+    }
+
+    @Override
+    public void initializeObject(Dependency object, QName name, Attributes attributes, XMLReader reader)
+            throws ObjectBuildException, XMLReadException {
+        attributes.getValue("id").ifPresent(object::setId);
+        attributes.getValue("type").ifPresent(object::setType);
+    }
+}
--- src/main/java/de/vcs/adapter/signal/SignalAdapter.java
+++ src/main/java/de/vcs/adapter/signal/SignalAdapter.java
@@ -1,8 +1,12 @@
 package de.vcs.adapter.signal;
 
 import de.vcs.adapter.helper.TextContentChecker;
+import de.vcs.model.odr.core.UnitEnumCollection;
 import de.vcs.model.odr.object.BorderType;
 import de.vcs.model.odr.signal.Signal;
+import de.vcs.util.ODRConstants;
+import org.xmlobjects.annotation.XMLElement;
+import org.xmlobjects.annotation.XMLElements;
 import org.xmlobjects.builder.ObjectBuildException;
 import org.xmlobjects.builder.ObjectBuilder;
 import org.xmlobjects.stream.XMLReadException;
@@ -11,6 +15,10 @@ import org.xmlobjects.xml.Attributes;
 
 import javax.xml.namespace.QName;
 
+@XMLElements({
+        @XMLElement(name = "signal",
+                namespaceURI = ODRConstants.ODR_1_6_NAMESPACE)
+})
 public class SignalAdapter implements ObjectBuilder<Signal> {
 
     @Override
@@ -30,10 +38,21 @@ public class SignalAdapter implements ObjectBuilder<Signal> {
         attributes.getValue("type").ifPresent(object::setType);
         attributes.getValue("subtype").ifPresent(object::setSubtype);
         attributes.getValue("value").ifDouble(object::setValue);
+        TextContentChecker.check(attributes.getValue("unit"), UnitEnumCollection.getUnitEnums(), object::setUnit);
+        attributes.getValue("height").ifDouble(object::setHeight);
+        attributes.getValue("width").ifDouble(object::setWidth);
+        attributes.getValue("text").ifPresent(object::setText);
     }
 
     @Override
     public void buildChildObject(Signal object, QName name, Attributes attributes, XMLReader reader)
             throws ObjectBuildException, XMLReadException {
+        if (ODRConstants.ODR_1_6_NAMESPACE.equals(name.getNamespaceURI())) {
+            switch (name.getLocalPart()) {
+                case "dependency":
+                    object.getDependencies().add(reader.getObjectUsingBuilder(DependencyAdapter.class));
+                    break;
+            }
+        }
     }
 }
